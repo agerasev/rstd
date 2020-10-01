e42fcae5b799c0c6f0a80c2d68076ee00201e7c5
Alexey Gerasev <alexey.gerasev@gmail.com>
2020-10-01 16:04:29 +0700

    Now Option can be either Some or None, no empty state

--- src/rstd/option.hpp
+++ src/rstd/option.hpp
@@ -12,18 +12,26 @@ private:
     Variant<Tuple<>, T> var;
 
 public:
-    Option() = default;
-    Option(Variant<Tuple<>, T> &&v) :
-        var(std::move(v))
-    {}
+    Option() : Option(Variant<Tuple<>, T>::template create<0>(Tuple<>())) {}
 
     Option(const Option &) = default;
     Option &operator=(const Option &) = default;
 
-    Option(Option &&) = default;
-    Option &operator=(Option &&) = default;
+    Option(Option &&other) : var(std::move(other.var)) {
+        other.var.template put<0>(Tuple<>());
+    }
+    Option &operator=(Option &&other) {
+        var.try_destroy();
+        var = std::move(other.var);
+        other.var.template put<0>(Tuple<>());
+    }
 
     ~Option() = default;
+
+private:
+    Option(Variant<Tuple<>, T> &&v) :
+        var(std::move(v))
+    {}
 
     const Variant<Tuple<>, T> &as_variant() const {
         return var;
@@ -32,8 +40,9 @@ public:
         return var;
     }
 
+public:
     static Option None() {
-        return Option(Variant<Tuple<>, T>::template create<0>(Tuple<>()));
+        return Option();
     }
     static Option Some(T &&x) {
         return Option(Variant<Tuple<>, T>::template create<1>(std::move(x)));
@@ -56,21 +65,47 @@ public:
         return this->var.template get<1>();
     }
 
+private:
+    T take_some() {
+#ifdef DEBUG
+        assert_(this->is_some());
+#endif // DEBUG
+        T v(std::move(this->var.template take<1>()));
+        this->var.template put<0>(Tuple<>());
+        return v;
+    }
+
+public:
+    Option take() {
+        if (this->is_some()) {
+            return Option<T>::Some(this->take_some());
+        } else {
+            return Option<T>::None();
+        }
+    }
+
     T unwrap() {
         if (!this->is_some()) {
-            panic_("Option unwrap error");
+            panic_("Option is None");
         }
-        return this->var.template take<1>();
+        return this->take_some();
     }
     T expect(const std::string &message) {
         if (!this->is_some()) {
-            panic_("Option expect none:\n{}", message);
+            panic_("Option expect Some:\n{}", message);
         }
-        return this->var.template take<1>();
+        return this->take_some();
     }
 
-    explicit operator bool() const {
-        return bool(var);
+    void unwrap_none() {
+        if (!this->is_none()) {
+            panic_("Option is Some");
+        }
+    }
+    void expect_none(const std::string &message) {
+        if (!this->is_none()) {
+            panic_("Option expect None:\n{}", message);
+        }
     }
 };
 
@@ -78,7 +113,6 @@ template <typename T>
 struct fmt::Display<Option<T>> {
 public:
     static void fmt(const Option<T> &t, std::ostream &o) {
-        assert_(bool(t));
         if (t.is_some()) {
             o << "Some(";
             write_(o, t.get());
