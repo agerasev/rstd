c0d4758bfdd660550c850b2d30edd5df03e6073a
Alexey Gerasev <alexey.gerasev@gmail.com>
2020-10-22 05:52:46 +0300

    Fix tests on MinGW

--- src/rtest/test.cpp
+++ src/rtest/test.cpp
@@ -10,7 +10,11 @@ rtest_module_(rtest) {
     rtest_should_panic_(panic) {
         panic_("Panic!");
     }
+#if defined(__MINGW32__) || defined(__MINGW64__)
+    // Cannot catch SegFault on MinGW
+#else
     rtest_should_panic_(segfault) {
         ((void(*)())nullptr)();
     }
+#endif
 }
